0114f359b5ddd95ece8058a9ff29328720840495
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-09-30 23:22:55 +0800

    typed.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: [
+        '<span style="color: #FF5733;">I\'m a PHP Developer</span>',
+        '<span style="color: #3498db;">With Laravel Framework</span>',
+        '<span style="color: #2ecc71;">I\'m an Application Developer</span>',
+        '<span style="color: #e74c3c;">I just started to Learn React.js</span>',
+        '<span style="color: #9b59b6;">Please check my skills below</span>'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+    smartBackspace: true 
+});
