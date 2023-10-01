3a874dc6e4b9d0b43783f66ae2b620c39a64f838
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-10-01 18:01:26 +0800

    work work work

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var typed = new Typed(".text", {
     strings: [
-        '<span style="color: #FF5733;">I\'m a PHP Developer</span>',
-        '<span style="color: #3498db;">With Laravel Framework</span>',
-        '<span style="color: #2ecc71;">I\'m an Application Developer</span>',
-        '<span style="color: #e74c3c;">I just started to Learn React.js</span>',
-        '<span style="color: #9b59b6;">Please check my skills below</span>'
+        '<span style="color: #fff;">I\'m a PHP Developer</span>',
+        '<span style="color: #c74254;">With Laravel Framework</span>',
+        '<span style="color: #fff;">I\'m an Application Developer</span>',
+        '<span style="color: #c74254;">I just started to Learn React.js</span>',
+        '<span style="color: #fff;">Please check my skills below</span>'
     ],
     typeSpeed: 50,
     backSpeed: 50,
