a9361afb3ae8c396a0a06951398a22cc37f2b50c
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-10-03 15:25:41 +0800

    styleswitcher

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/app.js', // Update this path to your app's entry point
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
