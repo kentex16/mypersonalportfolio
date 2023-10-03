77eed9109125f2a082d8156be3c1abcb409c9eb5
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-10-03 19:23:36 +0800

    hi

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var typed = new Typed(".text", {
     strings: [
         '<span style="color: #fff;">I\'m a Website Developer.</span>',
-        '<span style="color: #c74254;">With Laravel Framework.</span>',
+        '<span style="color: var(--red);">With Laravel Framework.</span>',
         '<span style="color: #fff;">I\'m not proficient in designing </span>',
-        '<span style="color: #c74254;">Thats why I started to Learn React.js.</span>',
-        '<span style="color: #c74254;">I want to learn more to improve myself. </span>',
+        '<span style="color: var(--red);">Thats why I started to Learn React.js.</span>',
+        '<span style="color: var(--red);">I want to learn more to improve myself. </span>',
         '<span style="color: #fff;">Please check my skills and interests below.</span>'
     ],
     typeSpeed: 50,
@@ -13,3 +13,50 @@ var typed = new Typed(".text", {
     loop: true,
     smartBackspace: true 
 });
+
+
+const anchorElements = document.querySelectorAll('a[data-content-id]');
+
+
+anchorElements.forEach(anchor => {
+    anchor.addEventListener('click', function (event) {
+        
+        event.preventDefault();
+
+       
+        const contentId = this.getAttribute('data-content-id');
+
+        
+        loadContent(contentId);
+    });
+});
+
+
+function loadContent(contentId) {
+    
+    
+    const contentMap = {
+        'lot-ecommerce': {
+            title: 'Lote Ecommerce',
+            description: ' Technologies used: PHP, Node.js, HTML, CSS, Bootstrap, Laravel, SQL, Tailwind CSS, jQuery, Pusher, AOS, JSON.'
+
+        },
+        
+    };
+
+    
+    const content = contentMap[contentId];
+
+    if (content) {
+        
+        const contentContainer = document.getElementById('content-container');
+        contentContainer.innerHTML = `
+            <h3>${content.title}</h3>
+            <p>${content.description}</p>
+            
+        `;
+    } else {
+        // Handle the case where content for the specified contentId is not found
+        console.error(`Content not found for ID: ${contentId}`);
+    }
+}
