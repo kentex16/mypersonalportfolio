f1a69b1ff9155c1ae2a9c04597b43c6413a5bfd4
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-10-04 12:28:14 +0800

    changes

---
+++ js/tailwind.config.js
@@ -0,0 +1,16 @@
+/** 
+ * @type {import('tailwindcss').Config} */
+
+module.exports = {
+  purge: [  './src/**/*.html',
+  './src/**/*.vue',
+  './src/**/*.jsx',],
+  darkMode: 'class', 
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
